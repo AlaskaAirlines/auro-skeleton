8118e1010e26e69461bbed7352cb88c15fef5e97
Gus Naughton <gus.naughton@alaskaair.com>
2021-05-12 09:30:31 -0700

    feat: initial component build

--- test/auro-skeleton.test.js
+++ test/auro-skeleton.test.js
@@ -1,20 +1,10 @@
 import { fixture, html, expect } from '@open-wc/testing';
-import sinon from 'sinon';
 import '../src/auro-skeleton.js';
 
 describe('auro-skeleton', () => {
-  it('sets the CSS class on auro-skeleton > div element', async () => {
-    const el = await fixture(html`
-      <auro-skeleton cssclass="testClass"></auro-skeleton>
-    `);
-
-    const div = el.shadowRoot.querySelector('div');
-    expect(div.className).to.equal('testClass');
-  });
-
   it('auro-skeleton is accessible', async () => {
     const el = await fixture(html`
-      <auro-skeleton cssclass="testClass"></auro-skeleton>
+      <auro-skeleton circle></auro-skeleton>
     `);
 
     await expect(el).to.be.accessible();
