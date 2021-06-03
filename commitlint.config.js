b07fcc76942c0b26357fe6db48320f1a42903940
Dale Sande <dale.sande@gmail.com>
2021-06-03 11:21:53 -0700

    fix(commitLint): add config; reset all max values

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    "header-max-length": [0, "always", 1000],
-  }
+    'body-max-line-length': [2, 'always', 120],
+    'footer-max-line-length': [2, 'always', 120],
+    'header-max-length': [2, 'always', 120],
+  },
 };
