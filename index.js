0a5deb789cb640410c32427a23753cbf1e2222ef
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-18 12:01:30 -0700

    feat(api): add register static method #39
    `AuroSkeleton.register` is to easily register the element without extra importing
    `import '@aurodesignsystem/auro-skeleton'` will still register this element to `<auro-skeleton>`
    `import { AuroSkeleton } from '../src/auro-skeleton.js'` wont register this element until `AuroSkeleton.register` gets called

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AuroSkeleton } from './src/auro-skeleton.js';
-import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-RuntimeUtils.default.prototype.registerComponent('custom-skeleton', AuroSkeleton);
+AuroSkeleton.register();
